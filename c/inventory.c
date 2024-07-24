3e2dfb37dea23b76595bd301fe3275b2fbfece85
jeproch <jeproch@gmail.com>
2024-07-24 16:43:15 +0200

    New render logic which still does not work

--- c/inventory.c
+++ c/inventory.c
@@ -9,19 +9,17 @@ SDL_Rect Inventory = {30, 200, 30, 30};
 
 bool bShowInventory = false;
 
-/*
-if (event.key.keysym.sym == SDLK_DOWN) {
-    if (player.y < ground.y - player.h) {
-      player.y += player.h;
-    }
-*/
-
 void openInventory() {
+
+  // write some logic to keep on rendering only if it should be rendering
   if (event.key.keysym.sym == SDLK_F2 && bShowInventory == false) {
     bShowInventory = true;
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue inventory
-    SDL_RenderFillRect(renderer, &Inventory);
   } else if (event.key.keysym.sym == SDLK_F2 && bShowInventory == true) {
     bShowInventory = false;
+  }
+
+  if (bShowInventory) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue inventory
+    SDL_RenderFillRect(renderer, &Inventory);
   }
 }
