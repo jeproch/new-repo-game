0f7ccb9c41db3cb41ff76edb59533162590d5839
jeproch <jeproch@gmail.com>
2024-07-26 18:48:52 +0200

    The inventory works now

--- c/inventory.c
+++ c/inventory.c
@@ -1,15 +1,18 @@
 #include "game.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_timer.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-SDL_Rect Inventory = {30, 200, 30, 30};
+SDL_Rect Inventory = {100, 30, 700, 400};
 bool bShowInventory = false;
 
 void toggleInventory() {
   if (event.key.keysym.sym == SDLK_F2) {
     SDL_Delay(100);
-    bShowInventory = !bShowInventory;
+    bShowInventory = true;
+  } else if (event.key.keysym.sym == SDLK_F3) {
+    bShowInventory = false;
   }
 }
