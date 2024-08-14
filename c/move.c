2ab3bce0409db783f7913c7eed50aa5009843231
jeproch <jeproch@gmail.com>
2024-08-14 13:57:56 +0200

    Functions to make sure 500 ms to show player [doesnt work]

--- c/move.c
+++ c/move.c
@@ -2,6 +2,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <stdbool.h>
+// #include <time.h>
 // #include <stdio.h>
 
 void keepPlayerOnWindow() {
@@ -21,16 +23,54 @@ void quitGame() { quit = 1; }
 
 // switch between animations when moving
 
+bool startedRight = false;
+int timeRight = 0;
+int endTimeRight;
+
+void startTimeShowRight() {
+  startedRight = true;
+  timeRight = SDL_GetTicks();
+  endTimeRight = timeRight + 500;
+}
+
+bool startedLeft = false;
+int timeLeft = 0;
+int endTimeLeft;
+
+void startTimeShowLeft() {
+  startedLeft = true;
+  timeLeft = SDL_GetTicks();
+  endTimeLeft = timeLeft + 500;
+}
+
 void move() { // SDL_KEYDOWN
   if (event.key.keysym.sym == SDLK_LEFT) {
     if (player.x > 0) {
       player.x -= 15;
-      showPlayerLeft();
+
+      if (startedLeft == false) {
+        startTimeShowLeft();
+      }
+
+      if (timeLeft != endTimeLeft) {
+        showPlayerLeft();
+      } else {
+        startedLeft = false;
+      }
     }
   } else if (event.key.keysym.sym == SDLK_RIGHT) {
     if (player.x < 1366 - player.w) {
       player.x += 15;
-      showPlayerRight();
+
+      if (startedRight == false) {
+        startTimeShowRight();
+      }
+
+      if (timeRight != endTimeRight) {
+        showPlayerRight();
+      } else {
+        startedRight = false;
+      }
     }
   } else if (event.key.keysym.sym == SDLK_F1) {
     quitGame();
